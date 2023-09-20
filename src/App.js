7ca24622cad7b52c05befee0d69feee105aafecf
Frank <fdoka00@gmail.com>
2023-09-20 16:07:40 -0400

    AWS Changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
-import Home from './pages/Home';
-import About from './pages/About';
 import PremiumPage from './pages/PremiumPage';
 
 function App() {
@@ -11,13 +8,11 @@ function App() {
     <div className="App">
       <Router>
         <div className="content">
-          <NavLink className="content"  exact activeClassName="active" to="/">Home</NavLink>
-          <NavLink className="content" activeClassName="active" to="/about">About</NavLink>
+          <NavLink className="content"  exact activeClassName="active" to="https://frankdoka.com/">Home</NavLink>
           <NavLink className="content" activeClassName="active" to="/premium">Premium Content</NavLink>
         </div>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<About />}></Route>
+          <Route path="/" element={<PremiumPage />}></Route>
           <Route path="/premium" element={<PremiumPage />}></Route>
         </Routes>
       </Router>
